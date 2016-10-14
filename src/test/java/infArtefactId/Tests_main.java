2eaa5ff954eb74ceaad574a17bb3373f417f9ec3
pklementowski <pklementowski@infusion.com>
2016-10-14 08:33:08 -0400

    Added 2 new tests

--- src/test/java/infArtefactId/Tests_main.java
+++ src/test/java/infArtefactId/Tests_main.java
@@ -1,19 +1,40 @@
 package infArtefactId;
 
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 
 public class Tests_main {
 	 public static RemoteWebDriver driver;
+	@BeforeClass
+	public void init(){
+		driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+	}
 	
 	@Test
-	public  void test1() {
-		driver = new FirefoxDriver();
-		 driver.manage().window().maximize();
-		driver.get("https://www.surveymonkey.com");
-		driver.findElementByXPath("//a[@class='sign-in']").click();
+	public  void checkPageIsOpen() {
+		driver.get("https://www.infusion.com");
+		Assert.assertEquals(driver.getTitle(),"Infusion | www.infusion.com");
+		WebElement footer = driver.findElementByXPath("//footer[contains(.,'Infusion')]");
+		Assert.assertTrue("Footer is missing!",footer.isDisplayed());
+	}
+	
+	@Test 
+	public void contentOfHamburgerMenu(){
+		driver.get("https://www.infusion.com");
+		WebElement menu = driver.findElementByXPath("//small[@class='INF-mainMenu_icon']");
+		Assert.assertTrue("Hamburger menu is disabled!",menu.isEnabled());
+		menu.click();
+		
+		List<WebElement> hamburgerContent = driver.findElementsByXPath("//ul[@class='INF-mainMenu_link']/li/a");
+		
 	}
 
 }
